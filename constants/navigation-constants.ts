8923677c35629dc3f0fdae4fb4cf295202b42e67
Codingvibes8 <wecodeappz@gmail.com>
2025-08-13 22:56:46 +0100

    refactor code

---
+++ constants/navigation-constants.ts
@@ -0,0 +1,6 @@
+export  const navigation = [
+    { name: "Men's Shoes", href: "/products?category=shoes" },
+    { name: "Men's Clothing", href: "/products?category=clothing" },
+    { name: "New Arrivals", href: "/products?sort=newest" },
+    { name: "Sale", href: "/products?sale=true" },
+]
